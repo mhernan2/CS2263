6f98394aedf5687e38aaacb612ea46d3e5ed965d
mhernan2@unb.ca <mhernan2@unb.ca>
2018-05-29 12:41:42 -0300

    made changes in code, got multiplication working, there's still a bug in the printing of the matrix that leads to a seg fault

--- homework/homework01/main.c
+++ homework/homework01/main.c
@@ -1,24 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "mymatrix.h"
 
+#define MULTIPLICATION "multiplication"
+#define ADDITION "addition"
+
 int main() {
-	int i, j, *matrix[5];
-	int *m = malloc(3*5*sizeof(int));
-
-	printf("allocating corresponding locations to array\n");
-	for (i = 0; i < 3; i++) {
-		matrix[i] = m+(5*i);
+	// scan the operation to be performed
+	char operation[30];
+	fgets(operation, 30, stdin);
+	// get the input values for matrix1
+	int i, r1, c1, r2, c2, **matrix1, **matrix2, *m1, *m2, **resultingMatrix;
+	scanf("%d%d\n", &r1, &c1);
+	matrix1 = malloc(r1*sizeof(int*));
+	m1 = malloc(r1*c1*sizeof(int));
+	for (i = 0; i < r1; i++) {
+		matrix1[i] = m1+(i*c1);
+	}
+	// read the values into the matrix
+	for (i = 0; i < r1*c1; i++) {
+		scanf("%d", &m1[i]);
 	}
 
-	printf("inserting values to matrix\n");
-	for (i = 0; i < 3; i++) {
-		for (j = 0; j < 5; j++) {
-			matrix[i][j] = i+j;
-		}
+	// get the input values for matrix2
+	scanf("%d%d\n", &r2, &c2);
+	matrix2 = malloc(r2*sizeof(int*));
+	m2 = malloc(r2*c2*sizeof(int));
+	for (i = 0; i < r2; i++) {
+		matrix2[i] = m2+(i*c2);
+	}
+	// read the values into the matrix
+	for (i = 0; i < r2*c2; i++) {
+		scanf("%d", &m2[i]);
+	}	
+
+	if(strcmp(operation, MULTIPLICATION)) {
+		resultingMatrix = matrix_mult(matrix1, r1, c1, matrix2, r2, c2);
+	} else if(strcmp(operation, ADDITION)) {
+		resultingMatrix = matrix_add(matrix1, r1, c1, matrix2, r2, c2);
+	} else {
+		printf("unknown operation \"%s\"\n", operation);
+		return 0;
 	}
 
-	// matrix[0][0] = 10;
-	printMatrix(matrix, 3, 5);
+	printMatrix(resultingMatrix);
+
 	return 0;
 }
--- homework/homework01/mymatrix.c
+++ homework/homework01/mymatrix.c
@@ -4,47 +4,66 @@
 #include "mymatrix.h"
 
 void printMatrix(int **m, int r, int c) {
+	printf("%d %d\n", r, c);
 	int i, j;
 	for (i = 0; i < r; i++) {
 		for (j = 0; j < c; j++) {
-			printf("%d ", m[i][j]);
+			printf("%d", m[i][j]);
+			if (j != c-1) printf(" ");
 		}
 		printf("\n");
 	}
 }
 
-int ** matrix_mult(int ** matrix1, int r1, int c1, int ** matrix2, int r2, int c2) {
-	// check if int ** is valid
-	if (c1 != r2) {
-		return 0; 
-	}
-
-	// create resulting int **
-	int i, j, k;
-	int **resultingMatrix = malloc(r1*c2*sizeof(int));
-
-	return resultingMatrix;
-
-}
 
 // returns an the row vector of a int ** in the form of an array
 // 0 is for rows and 1 for column vector
-int *getRowVector(int roc, int index, const int length, int **m) {
-	int *vector;
+int *getColumnVector(int index, int length, int **m) {
+	int i, *vector = malloc(length*sizeof(int));
 
-	if (roc == 0) {
-		 vector = m[index];
-	} else if (roc == 1) {
-		int i;
-		for (i = 0; i < length; i++) {
-			vector[i] = m[i][index];
-		}
+	for (i = 0; i < length; i++) {
+		vector[i] = m[i][index];
 	}
 
 	return (vector);
 }
 
-int **matrix_add(int** matrix1, int ** matrix2) {
+
+int ** matrix_mult(int ** matrix1, int r1, int c1, int ** matrix2, int r2, int c2) {
+	// check if int ** is valid
+	if (c1 != r2) {
+		return 0;
+	}
+
+	// create resulting int **
+	int i, j, k, **resultingMatrix = malloc(r1*sizeof(int));
+	int *rm = malloc(r1*c2*sizeof(int));
+
+	for (i = 0; i < r1; i++) {
+		resultingMatrix[i] = rm+(i*c2);
+	}
+
+	for (i = 0; i < c2; i++) {
+		for (j = 0; j < r1; j++) {
+			int result = 0;
+			int *column = getColumnVector(i, r2, matrix2);
+
+			for (k = 0; k < c1; k++) {
+				result += matrix1[j][k] * column[k];
+			}
+			printf("contents of resultingMatrix[%d][%d]: %d\n", j, i, result);
+			resultingMatrix[j][i] = result;
+		}
+	}
+
+	printf("printing resulting matrix\n");
+	printMatrix(resultingMatrix, r1, c2);
+	return resultingMatrix;
+
+}
+
+
+int ** matrix_add(int **matrix1, int r1, int c1, int **matrix2, int r2, int c2) {
 	int **a = malloc(4*sizeof(int*));
 	return a;
 }
